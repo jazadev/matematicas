f89efa398f4ba07c1d5a92bc0b9fbb3531a32209
Jorge Zúñiga <37384725+jazadev@users.noreply.github.com>
2025-04-29 19:02:03 -0600

    Agregar multiplicación y división

--- calculadora.js
+++ calculadora.js
@@ -1,13 +1,31 @@
 /*
-Función suma
+* Función suma
 */
 function sumar(a, b) {
     return a + b;
 }
 
 /*
-Función resta
+* Función resta
 */
 function restar(a, b) {
     return a - b;
+}
+
+/** 
+ * Función multiplicar
+ */
+function multiplicar(a, b) {
+    return a * b;
+}
+
+/**
+ * Función Dividir
+ * @param {F} a 
+ * @param {*} b 
+ * @returns 
+ */
+function dividir(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
 }
